c2afc97911d9c0d893c7913f2ed8c8ebf5974483
Ash <ashutosh2564@gmail.com>
2022-09-05 10:19:29 +0530

    Update Use.js

--- src/Components/Use.js
+++ src/Components/Use.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './main.css';
 
 
 const Use = () => {
+  const [btnState, setBtnState] = useState(false);
+
+  const handleClick = () => {
+    setBtnState(!btnState)
+  }
+
+
   return (
     <div>
     <h1>How are you planning to use Eden?</h1>
     <p className="p">We'll streamline your setup experience accordingly.</p>
-    <button className="dialog"> <h1 className="dia">For myself</h1><p>Write better. Think more clearly. Stay organized.</p>
-    </button>
-    <button className="dialog"> <h1 className="dia">With my team</h1> <p>Wikis, docs, tasks & projects, all in one place.</p>
-    </button>
+    <button
+        className={btnState ? 'activate' : 'nonactivate'}
+        onClick={handleClick}
+      ><h1 className="dia">For myself</h1><p>Write better. Think more clearly. Stay organized.</p></button>
+    <button
+        className={btnState ? 'activate' : 'nonactivate'}
+        onClick={handleClick}
+      ><h1 className="dia">With my team</h1> <p>Wikis, docs, tasks & projects, all in one place.</p></button>
+ 
     </div>
   )
 }
