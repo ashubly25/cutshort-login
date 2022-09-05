b6920084248254ec945b05d47362020592f9e234
Ash <ashutosh2564@gmail.com>
2022-09-05 08:34:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Progress from './Components/Progress';
+import "@fontsource/inter";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Progress />
+    </div>
+  );
+}
+
+export default App;
